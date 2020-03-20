1b6623410fc4a957ccf3ae3114795c470d44fbb7
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2020-03-19 23:51:08 -0400

    Started work on the templates, base template mostly done.

--- mellyn/settings.py
+++ mellyn/settings.py
@@ -117,4 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static_files')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
