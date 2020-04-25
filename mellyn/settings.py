f7b1ba65ecfc1ffdb4609396b5fb26f815e5a2ca
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2020-04-25 14:20:39 -0400

    add django-guardian to settings

--- mellyn/settings.py
+++ mellyn/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bleach',
+    'guardian',
     'agreements.apps.AgreementsConfig',
     'accounts.apps.AccountsConfig',
 ]
@@ -52,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'guardian.backends.ObjectPermissionBackend',
+)
 
 ROOT_URLCONF = 'mellyn.urls'
 
