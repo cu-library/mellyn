8e58ddbbdbc62757f27e9eed02a0f7ee19668cc1
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2020-04-25 14:17:15 -0400

    Run html validator on all html inputs

--- static/js/main.js
+++ static/js/main.js
@@ -16,12 +16,6 @@
             document.getElementById('id_slug').value = window.URLify(this.value);
         });
     }
-    const resourceInput = document.querySelector('form.create #id_resource');
-    if(resourceInput){
-        resourceInput.addEventListener('input', function (e){
-            document.getElementById('id_resource_slug').value = window.URLify(this.value);
-        });
-    }
 })();
 
 // Set and remove an error on a form element.
@@ -64,51 +58,54 @@
 
 })();
 
-// Check the validity of an agreement body.
-(function checkBodyValid(){
+// Check the validity of an HTML input.
+(function checkHTMLInputValid(){
     'use strict';
 
-    // Does this page have the body text area?
-    const bodyInput = document.getElementById('id_body');
-    // If not, return early.
-    if(!bodyInput){
-        return;
-    }
-
-    const INVALID_MESSAGE_ID = 'id_body_error_message_invalid_html';
-    const parser = new DOMParser();
-
-    // allowedTags is extracted from the model and passed through the view.
-    const allowedTags = bodyInput.dataset.allowedTags.split(',');
-
-    bodyInput.addEventListener('input', function (e){
-        // Parse the body text.
-        let doc = parser.parseFromString(this.value, 'text/html');
-        // Did the parser return an error document?
-        let parseErrors = doc.getElementsByTagName('parsererror');
-        if(parseErrors.length > 0){
-            window.setErrorOnElement('id_body', INVALID_MESSAGE_ID, 'Invalid HTML.');
-            return;
-        }
-        // Did the parser have to 'fix' the input?
-        if(doc.body.innerHTML!==this.value){
-            window.setErrorOnElement('id_body', INVALID_MESSAGE_ID, 'Invalid HTML.');
-            return;
-        }
-        // Did the parser encounter any 'bad' tags?
-        let allElems = doc.body.getElementsByTagName('*');
-        for(const elem of allElems){
-            const tag = elem.tagName.toLowerCase();
-            if(!allowedTags.includes(tag)){
-                window.setErrorOnElement('id_body', INVALID_MESSAGE_ID, 'Invalid HTML tag: ' + tag);
+    function addValidationEventListener(element){
+        element.addEventListener('input', function (e){
+            const INVALID_MESSAGE_ID = `id_${this.id}_error_message_invalid_html`;
+            const parser = new DOMParser();
+            // allowedTags is extracted from the model and passed through the view.
+            const allowedTags = this.dataset.allowedTags.split(',');
+            // Parse the text.
+            let doc = parser.parseFromString(this.value, 'text/html');
+            // Did the parser return an error document?
+            let parseErrors = doc.getElementsByTagName('parsererror');
+            if(parseErrors.length > 0){
+                window.setErrorOnElement(this.id, INVALID_MESSAGE_ID, 'Invalid HTML.');
                 return;
             }
-        }
-        // All good, remove previous errors if found.
-        window.removeErrorOnElement('id_body', INVALID_MESSAGE_ID);
-    });
+            // Did the parser have to 'fix' the input?
+            if(doc.body.innerHTML!==this.value){
+                window.setErrorOnElement(this.id, INVALID_MESSAGE_ID, 'Invalid HTML.');
+                return;
+            }
+            // Did the parser encounter any 'bad' tags?
+            let allElems = doc.body.getElementsByTagName('*');
+            for(const elem of allElems){
+                const tag = elem.tagName.toLowerCase();
+                if(!allowedTags.includes(tag)){
+                    window.setErrorOnElement(this.id, INVALID_MESSAGE_ID, 'Invalid HTML tag: ' + tag);
+                    return;
+                }
+            }
+            // All good, remove previous errors if found.
+            window.removeErrorOnElement(this.id, INVALID_MESSAGE_ID);
+        });
+    }
 
-    // Run it once, manually, on page load.
-    var event = new Event('input');
-    bodyInput.dispatchEvent(event);
+    const htmlInputsIds = ['id_body', 'id_description'];
+    for (const htmlInputId of htmlInputsIds) {
+        // Does this page have an HTML text area?
+        const htmlInput = document.getElementById(htmlInputId);
+        // If not, return early.
+        if(!htmlInput){
+            continue;
+        }
+        addValidationEventListener(htmlInput);
+        // Run it once, manually, on page load.
+        var event = new Event('input');
+        htmlInput.dispatchEvent(event);
+    }
 })();
