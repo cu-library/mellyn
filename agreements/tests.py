a1fe80bebb367c90e48627394c7a4ec703688e7e
Kevin Bowrin <kevinbowrin@cunet.carleton.ca>
2020-05-16 22:32:46 -0400

    Add new tests for agreement and resource pagination and permission checks

--- agreements/tests.py
+++ agreements/tests.py
@@ -5,9 +5,13 @@ https://docs.djangoproject.com/en/3.0/topics/testing/
 """
 
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.contrib.auth.models import Group, Permission
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
+from guardian.shortcuts import assign_perm
+
+from .views import AgreementList, ResourceList
 from .models import Resource, Faculty, Department, Agreement
 
 
@@ -199,3 +203,173 @@ class SharedCRUDWorkflowsTestCase(TestCase):
             response = self.client.get(reverse(lowercase_plural+'_update', args=['test']))
             with self.subTest(msg=lowercase_plural+'_update'):
                 check_label_suffix(response)
+
+
+class PaginationTestCase(TestCase):
+    """Test pagination"""
+
+    def setUp(self):
+        """Create a test user"""
+        self.user = get_user_model().objects.create_user(username='user',
+                                                         first_name='test',
+                                                         last_name='test',
+                                                         email='admin@test.com',
+                                                         password='test')
+
+    def test_agreement_pagination(self):
+        """Test agreement pagination"""
+        test_resource = Resource.objects.create(name='Test', slug='test', description='')
+        for i in range(35):
+            Agreement.objects.create(title=f'Test-{i}',
+                                     slug=f'test-{i}',
+                                     resource=test_resource,
+                                     body='body',
+                                     redirect_url='https://example.com',
+                                     redirect_text='example-redirect',
+                                     hidden=((i % 10) == 0))  # 0, 10, 20, 30 are hidden
+        # Test HTML
+        self.client.login(username='user', password='test')
+        response = self.client.get(reverse('agreements_list'))
+        self.assertContains(response, '<span class="current">Page 1 of 3.</span>', html=True)
+        # Test view context
+        request = RequestFactory().get(reverse('agreements_list'))
+        request.user = self.user
+        agreement_list = AgreementList()
+        agreement_list.setup(request)
+        context = agreement_list.get_context_data(object_list=agreement_list.get_queryset())
+        self.assertEqual(context['paginator'].count, 31)
+
+    def test_resource_pagination(self):
+        """Test resource pagination"""
+        for i in range(67):
+            Resource.objects.create(name=f'Test-{i}', slug=f'test-{i}', description='', hidden=((i % 10) == 0))
+
+        # Test HTML
+        self.client.login(username='user', password='test')
+        response = self.client.get(reverse('resources_list'))
+        self.assertContains(response, '<span class="current">Page 1 of 4.</span>', html=True)
+        # Test view context
+        request = RequestFactory().get(reverse('resources_list'))
+        request.user = self.user
+        resource_list = ResourceList()
+        resource_list.setup(request)
+        context = resource_list.get_context_data(object_list=resource_list.get_queryset())
+        self.assertEqual(context['paginator'].count, 60)
+
+
+class HiddenAgreementResourceTestCase(TestCase):
+    """Test the resource and agreement listings which limit visibility of hidden objects"""
+
+    models = ('agreement', 'resource')
+
+    def setUp(self):
+        """Create test data for this test case"""
+        self.test_staff_1 = get_user_model().objects.create_user(username='staff1',
+                                                                 first_name='test',
+                                                                 last_name='test',
+                                                                 email='staff1@test.com',
+                                                                 password='test',
+                                                                 is_staff=True)
+        self.test_staff_2 = get_user_model().objects.create_user(username='staff2',
+                                                                 first_name='test',
+                                                                 last_name='test',
+                                                                 email='staff2@test.com',
+                                                                 password='test',
+                                                                 is_staff=True)
+        self.test_patron = get_user_model().objects.create_user(username='patron',
+                                                                first_name='test',
+                                                                last_name='test',
+                                                                email='patron@test.com',
+                                                                password='test')
+        self.test_group = Group.objects.create(name='test')
+        self.test_resource = Resource.objects.create(name='Test Resource WooHoo', slug='test', description='')
+        self.test_agreement = Agreement.objects.create(title='Test Agreement WooHoo',
+                                                       slug='test',
+                                                       resource=self.test_resource,
+                                                       body='body',
+                                                       redirect_url='https://example.com',
+                                                       redirect_text='example-redirect')
+
+        self.object_per_model = {'agreement': self.test_agreement, 'resource': self.test_resource}
+
+    def object_visible(self, model, hidden_label=False):
+        """Is the test instance of the model visible, with or without the hidden label"""
+        response = self.client.get(reverse(f'{model}s_list'))
+        if hidden_label:
+            self.assertContains(response, f'Test {model.title()} WooHoo (Hidden)')
+        else:
+            self.assertContains(response, f'Test {model.title()} WooHoo')
+        response = self.client.get(reverse(f'{model}s_read', args=['test']))
+        self.assertContains(response, f'Test {model.title()} WooHoo')
+
+    def object_hidden(self, model):
+        """Is the test instance of the model hidden"""
+        response = self.client.get(reverse(f'{model}s_list'))
+        self.assertContains(response, f'<p>No {model}s found.</p>', html=True)
+        self.assertNotContains(response, f'Test {model.title()} WooHoo')
+        response = self.client.get(reverse(f'{model}s_read', args=['test']))
+        self.assertEqual(response.status_code, 403)
+
+    def test_object_hidden(self):
+        """Test to make sure agreements and resources are hidden properly"""
+        for model in self.models:
+            with self.subTest(msg=model+'_test_object_hidden'):
+                # Can a patron see the object before it is hidden?
+                self.client.login(username='patron', password='test')
+                self.object_visible(model)
+
+                # Hide the object
+                self.object_per_model[model].hidden = True
+                self.object_per_model[model].save()
+
+                # The patron should no longer be able to see the object
+                self.object_hidden(model)
+
+    def test_object_global_permissions(self):
+        """Test that global group permissions allow a user to see a hidden object"""
+        for model in self.models:
+            with self.subTest(msg=model+'_test_agreement_global_permissions'):
+                self.object_per_model[model].hidden = True
+                self.object_per_model[model].save()
+
+                # Add staff1 to the test group.
+                self.test_group.user_set.add(self.test_staff_1)
+
+                # They shouldn't be able to see the object.
+                self.client.login(username='staff1', password='test')
+                self.object_hidden(model)
+
+                # Give the group the global permission.
+                view_model = Permission.objects.get(codename=f'view_{model}')
+                self.test_group.permissions.add(view_model)
+
+                # They should now be able to see the agreement
+                self.object_visible(model, hidden_label=True)
+
+                # It should still be hidden for other staff members
+                self.client.login(username='staff2', password='test')
+                self.object_hidden(model)
+
+    def test_agreement_object_permissions(self):
+        """Test that object group permissions allow a user to see a hidden object"""
+        for model in self.models:
+            with self.subTest(msg=model+'_test_agreement_object_permissions'):
+                self.object_per_model[model].hidden = True
+                self.object_per_model[model].save()
+
+                # Add staff1 to the test group.
+                self.test_group.user_set.add(self.test_staff_1)
+
+                # They shouldn't be able to see the object.
+                self.client.login(username='staff1', password='test')
+                self.object_hidden(model)
+
+                # Give the group the object permission.
+                assign_perm(f'view_{model}', self.test_group, self.object_per_model[model])
+
+                # They should now be able to see the agreement
+                self.object_visible(model, hidden_label=True)
+
+                # It should still be hidden for other staff members
+                self.client.login(username='staff2', password='test')
+                self.object_hidden(model)
